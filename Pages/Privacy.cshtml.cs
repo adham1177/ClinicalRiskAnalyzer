91827eda0e02eb895f760183762ec14b0cb701cb
Adham Ayman <adhamayman797@gmail.com>
2026-03-04 15:40:44 +0200

    Initial project setup with ASP.NET Core Razor Pages

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClinicalRiskAnalyzer.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
